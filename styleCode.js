73388f5266131daa98f740c6546339617a93a959
Matthew Sojourner Newton <matt@knosis.org>
2012-08-07 19:31:03 -0700

    added google-code-prettify to repo

--- styleCode.js
+++ styleCode.js
@@ -1,20 +1,34 @@
-function styleCode() 
-{
- if (typeof disableStyleCode != "undefined") 
- {
-   return;
- }
-var a = false;
- $("pre code").parent().each(function() 
- {
-   if (!$(this).hasClass("prettyprint")) 
-   {
-       $(this).addClass("prettyprint");
-       a = true
-   }
- });
- if (a) { prettyPrint() } 
+function addClass( classname, element ) {
+    var cn = element.className;
+    //test for existance
+    if( cn == "undefined" ) {
+        return;
+    }
+    //add a space if the element already has class
+    if( cn != '' ) {
+        classname = ' ' + classname;
+    }
+    element.className = cn + classname;
 }
-$(function() {
-   styleCode();
-});
+
+
+function styleCode()
+{
+  if (typeof disableStyleCode != "undefined") 
+  {
+    return;
+  }
+  var a = false;
+  // iterate through all <pre> tags and add "prettyprint" class
+  var matches = document.body.getElementsByTagName("pre");
+  for (var i = 0; i < matches.length; ++i)
+  {
+    addClass("prettyprint", matches[i]);
+    a = true;
+  }
+  if (a) { prettyPrint() } 
+}
+
+
+
+window.onload = function() { styleCode(); };
